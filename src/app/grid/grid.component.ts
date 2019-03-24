fb192bea78a03ebe03d669acd3c6e37918c6cc1c
mpascualach <miguelpacheson@hotmail.es>
2019-03-24 23:18:22 +0100

    fleshed out sorting options

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -14,22 +14,53 @@ const gridPortionObject = require('./gridportion.json');
 
 export class GridComponent implements OnInit {
   grid = gridPortionObject;
+  originalGrid: Array<any>;
+  selected: string;
+
 
   constructor( public http: HttpClient ) {
-
+    this.originalGrid = this.grid;
+    this.originalGrid.forEach(m => {
+      console.log(m.city);
+    })
   }
 
   ngOnInit() {
-    // console.log(gridObject);
-    console.log(gridPortionObject);
+    
   }
 
-  sortGrid(){
+  sortGrid(sortBy){
 
+    switch(sortBy){
+      case 'city':
+        this.grid = this.originalGrid.sort(( a, b ) => a.city.localeCompare(b.city) );
+        break;
+      case 'start-date':
+        this.grid = this.sortDates(this.originalGrid, true);
+        break;
+      case 'end-date':
+        this.grid = this.sortDates(this.originalGrid, true);
+        break;
+      case 'price':
+        this.grid = this.originalGrid.sort(( a, b ) => a.price - b.price );
+        break;
+      case 'status':
+      case 'color':
+        this.grid = this.originalGrid.sort(( a, b ) => a.color.localeCompare(b.color) );
+    }
+  }
+
+  sortDates(array, start){
+    let sorted = array.sort(function (a, b) { 
+      var aa = a.split('/').reverse().join(),
+      bb = b.split('/').reverse().join();
+      return aa < bb ? -1 : (aa > bb ? 1 : 0);
+    });
+    return start ? sorted : sorted.reverse();
   }
 
   filterDates(){
-    
+
   }
 
 }
