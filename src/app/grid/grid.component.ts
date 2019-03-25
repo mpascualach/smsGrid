d9468dd5663c88d2a00032868d001f5075a90d9b
mpascualach <miguelpacheson@hotmail.es>
2019-03-25 11:18:44 +0100

    fixed end-date sort function

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -16,6 +16,7 @@ export class GridComponent implements OnInit {
   grid = gridPortionObject;
   originalGrid: Array<any>;
   selected: string;
+  reversed: boolean = false;
 
 
   constructor( public http: HttpClient ) {
@@ -27,37 +28,46 @@ export class GridComponent implements OnInit {
   }
 
   sortGrid(sortBy){
-
-    switch(sortBy){
-      case 'city':
-        this.grid = this.originalGrid.sort(( a, b ) => a.city.localeCompare(b.city) );
-        break;
-      case 'start-date':
-        this.grid = this.sortDates(this.originalGrid, true);
-        break;
-      case 'end-date':
-        this.grid = this.sortDates(this.originalGrid, false);
-        break;
-      case 'price':
-        this.grid = this.originalGrid.sort(( a, b ) => a.price - b.price );
-        break;
-      case 'status':
-        this.grid = this.originalGrid.sort(( a, b ) => a.status.localeCompare(b.status))
-      case 'color':
-        this.grid = this.originalGrid.sort(( a, b ) => a.color.localeCompare(b.color) );
+    if (this.selected !== sortBy){
+      this.reversed = false;
+      this.selected = sortBy;
+      switch(sortBy){
+        case 'city':
+          this.grid = this.originalGrid.sort(( a, b ) => a.city.localeCompare(b.city) );
+          break;
+        case 'start-date':
+          this.grid = this.sortDates(this.originalGrid, true);
+          break;
+        case 'end-date':
+          this.grid = this.sortDates(this.originalGrid, false);
+          break;
+        case 'price':
+          this.grid = this.originalGrid.sort(( a, b ) => a.price - b.price );
+          break;
+        case 'status':
+          this.grid = this.originalGrid.sort(( a, b ) => a.status.localeCompare(b.status))
+        case 'color':
+          this.grid = this.originalGrid.sort(( a, b ) => a.color.localeCompare(b.color) );
+      }
+    }
+    else {
+      this.reversed = true;
+      this.grid = this.grid.reverse(); 
     }
   }
 
   sortDates(array, start){
     let sorted = array.sort(function (a, b) { 
-      let aa = start ? a.start_date.split('/').reverse().join() : a.end_date.split("/");
-      let bb = start ? b.start_date.split('/').reverse().join() : b.end_date.split("/");
+      let aa = start ? a.start_date.split('/').reverse().join() : a.end_date.split("/").reverse().join();
+      let bb = start ? b.start_date.split('/').reverse().join() : b.end_date.split("/").reverse().join();
       return aa < bb ? -1 : (aa > bb ? 1 : 0);
     });
     return start ? sorted : sorted.reverse();
   }
 
-  
+  onChange(){
+    
+  }
 
   filterDates(){
 
