cc537b224aa94249030c3aa19435df30e14e6a16
mpascualach <miguelpacheson@hotmail.es>
2019-03-26 22:07:39 +0100

    fixed registration form final step

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Validators, FormBuilder, FormGroup, FormControlName, FormControl, AbstractControl, ValidatorFn } from '@angular/forms';
 
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
@@ -8,40 +10,40 @@ import { Validators, FormBuilder, FormGroup, FormControlName, FormControl, Abstr
 })
 
 export class RegistrationComponent implements OnInit {
-  userData: FormGroup;
+  name: string = '';
+  password: string = '';
+  confirmPassword: string = '';
 
-  name: FormControl;
-  password: FormControl;
-  confirmPassword: FormControl;
+  nameValid: boolean = false;
+  passwordValid: boolean = false;
+  confirmPasswordValid: boolean = false;
 
-  constructor( private fb: FormBuilder ) { 
+  constructor( private fb: FormBuilder, private router: Router ) { 
     
   }
 
-  checkPasswords(group: FormGroup) { // here we have the 'passwords' group
-    let pass = group.controls.password.value;
-    let confirmPass = group.controls.confirmPassword.value;
-
-    return pass === confirmPass ? null : { notSame: true }     
+  ngOnInit() {
+    
   }
 
-  checkLimit(min: number, max: number): ValidatorFn {
-    return (c: AbstractControl): { [key: string]: boolean } | null => {
-        if (c.value && (isNaN(c.value) || c.value < min || c.value > max)) {
-            return { 'range': true };
-        }
-        return null;
-    };
+  onChange(e, detail){
+    switch(detail){
+      case 'name':
+        this.name = e;
+        this.nameValid = this.name.length < 2 ? false : true;
+        break;
+      case 'password':
+        this.password = e;
+        this.passwordValid = this.password.length < 8 ? false : true;
+        break;
+      case 'confirmPassword':
+        this.confirmPassword = e;
+        this.confirmPasswordValid = this.confirmPassword !== this.password ? false : true;
+        break;
+    }
   }
 
-  public ngOnInit() {
-    this.userData = this.fb.group({
-      name: ['', this.checkLimit(8,8)],
-      password: new FormControl('', [Validators.maxLength(8)]),
-      confirmPassword: [''] }, { validator: this.checkPasswords })
-  }
-
-  login(form){
-    console.log(form);
+  signUp(){
+    this.router.navigate(['/grid']);
   }
 }
